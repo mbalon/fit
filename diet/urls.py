f15794f2201b03cfda1c2f0283bd2730752704c2
mbalon <balonmateusz@gmail.com>
2021-01-23 14:29:43 +0100

    fix bug

--- diet/urls.py
+++ diet/urls.py
@@ -7,7 +7,7 @@ router.register(r'recipe', RecipeViewSet)
 router.register(r'rating', RatingViewSet)
 router.register(r'ingredient', IngredientViewSet)
 
-urls_pattern = [
+urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
